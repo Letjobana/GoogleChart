e5d68a485f9ba5d28885ce6367c4abd21a95f210
Suzan Masite <SuzanM@blts.co.za>
2023-04-11 15:41:19 +0200

    Updated the code to add a bar chart for the student

---
+++ GoogleChart/Controllers/VisualizeController.cs
@@ -0,0 +1,52 @@
+﻿using GoogleChart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoogleChart.Controllers
+{
+    public class VisualizeController : Controller
+    {
+        // GET: Visualize
+        public ActionResult ColumnChart()
+        {
+            return View();
+        }
+        public ActionResult VisualizeStudentResult()
+        {
+            return Json(Result(),JsonRequestBehavior.AllowGet);
+        }
+        public List<StudentResult> Result()
+        {
+            List<StudentResult>studentResults= new List<StudentResult>();
+            studentResults.Add(new StudentResult()
+            {
+                studentName = "Atar",
+                markObtained = 88
+            });
+            studentResults.Add(new StudentResult()
+            {
+                studentName = "Qasim",
+                markObtained = 66
+            });
+            studentResults.Add(new StudentResult()
+            {
+                studentName = "Abdul Hanan",
+                markObtained = 77
+            });
+            studentResults.Add(new StudentResult()
+            {
+                studentName = "Haaanain Ahmad",
+                markObtained = 95
+            });
+            studentResults.Add(new StudentResult()
+            {
+                studentName = "Hazma",
+                markObtained = 90
+            });
+            return studentResults;
+        }
+    }
+}
---
+++ GoogleChart/Models/StudentResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoogleChart.Models
+{
+    public class StudentResult
+    {
+        public string studentName { get; set; }
+        public int markObtained { get; set; }
+    }
+}
